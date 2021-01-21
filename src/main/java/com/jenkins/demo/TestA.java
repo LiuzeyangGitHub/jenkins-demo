6ceb773f0af5affd739352ddf64d43efda63028f
liuzeyanga <liuzeyanga@zbj.com>
2021-01-21 12:27:59 +0800

    test

--- src/main/java/com/jenkins/demo/TestA.java
+++ src/main/java/com/jenkins/demo/TestA.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestA {
     @GetMapping("/test")
     public String get(){
-        return "succC";
+        return "succD";
     }
 }
